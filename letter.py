41b5773a60026a12b39b83527f7ff19385cfba98
Pavan-Kumar-Mallela <167163832+Pavan-Kumar-Mallela@users.noreply.github.com>
2025-01-24 20:34:03 +0530

    Create letter.py

---
+++ letter.py
@@ -0,0 +1,7 @@
+# Write code below 💖
+# this is  a letter to my future self in a month 
+print("Today's Date: 24 JAN 2025")
+print('I am feeling good and started my revisit of python basics and revisiting everything from basics is kind of nice feeling')
+print('I wanted to revisit the python to build a stronger foundation in thos language to make it easier for shifts in future')
+print('Hey future me you really think we did the right thing ')
+print('😊')

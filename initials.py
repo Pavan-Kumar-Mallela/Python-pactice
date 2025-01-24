840942e85d91442a114d0da5ecffa8f9457e910d
Pavan-Kumar-Mallela <167163832+Pavan-Kumar-Mallela@users.noreply.github.com>
2025-01-24 20:25:56 +0530

    Create initials.py

---
+++ initials.py
@@ -0,0 +1,8 @@
+# Write code below 💖
+#my name is Mallela Pavan Kumar
+print('M       M   PPPPPP   K     K')
+print('M M   M M   P     P  K    K ')
+print('M  M M  M   P     P  K   K  ')
+print('M   M   M   PPPPPP   K k    ')
+print('M       M   P        K   K  ')
+print('M       M   P        K    K ')

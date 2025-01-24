38f1069c9a8ecde67bcaad419aca5e61714a2af0
Pavan-Kumar-Mallela <167163832+Pavan-Kumar-Mallela@users.noreply.github.com>
2025-01-24 20:45:26 +0530

    Create variables_freebie.py

---
+++ variables_freebie.py
@@ -0,0 +1,15 @@
+name = 'Solrider'
+user_id = 16180339887
+progress = 0.75
+exp = 60
+verified = True
+xp = 70
+xp = 80
+
+print(xp)    # Output: 80
+pi = 3.14159
+meal_cost = 12.99
+message = "good nite"
+username = '@snoopdogg'
+late_to_class = False
+cranky = True
